f43b3315fcb821e6a2e04d0abefaed7c98d57c20
stromtrooper01 <57230643+stromtrooper01@users.noreply.github.com>
2021-05-27 16:29:32 +0200

    Delete new book directory

--- new book/new book/hash.rb
+++
@@ -1,5 +0,0 @@
-my_name  = {name: 'Nikita', age: 21}
-her_name = {name: 'Eva', age: 20}
-
-puts my_name[:name]
-p her_name[:age]
--- new book/new book/pin.rb
+++
@@ -1,6 +0,0 @@
-def validate_pin(pin)
-    pin = Array 
-    while pin.length != 4 || pin.length != 6 
-      return false
-      end
-  end
